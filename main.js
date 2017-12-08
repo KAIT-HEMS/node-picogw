ceb04208d8c8293d7a1fc6c56f75505c7d2f0df2
Satoshi Ikeda <ikeda@koozyt.com>
2017-12-08 22:09:58 +0900

    Apply coding style

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
 // PicoGW = Minimalist's Home Gateway
 const controller = require('./lib/controller');
-const PubSub = require('./lib/PubSub').PubSub;
+const PubSub = require('./lib/pub-sub').PubSub;
 const log = console.log;
 
 // Support for termux
-if( process.platform == 'android' )
-	Object.defineProperty(process, "platform", { get: function () { return 'linux'; } });
+if (process.platform == 'android') {
+    Object.defineProperty(process, 'platform', {get: function() {
+        return 'linux';
+    }});
+}
 
 // Parse command line
-var cmd_opts = require('opts');
-cmd_opts.parse([
+let cmdOpts = require('opts');
+cmdOpts.parse([
     {
         'short': 'p',
         'long': 'port',
         'description': 'Web API port number',
         'value': true,
-        'required': false
+        'required': false,
     },
     {
         'long': 'pipe',
-        'description': 'path of named pipes without postfix (_r or _w). The server is blocked until the pipe client is connected.',
+        'description': 'path of named pipes without postfix (_r or _w).'
+            + 'The server is blocked until the pipe client is connected.',
         'value': true,
-        'required': false
+        'required': false,
     },
-],true);
+], true);
 
 
-controller.init({PubSub:PubSub,cmd_opts:cmd_opts}).then(re=>{
-    log('Plugins have been initialized.') ;
-}).catch(e => {
+controller.init({PubSub: PubSub, cmd_opts: cmdOpts}).then((re)=>{
+    log('Plugins have been initialized.');
+}).catch((e) => {
     console.error(e);
-})
+});
 
-log('PicoGW started.') ;
+log('PicoGW started.');
