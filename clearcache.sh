75da3845a78d288fa69d7746a3f560ddba59d964
Shigeru Owada <sowd@acm.org>
2017-12-22 14:43:35 +0900

    Still debuging plugin-interface-net

--- clearcache.sh
+++ clearcache.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
+rm -rf storage
 
-find ./ -name "localstorage.json" -exec rm {} \;
-rm ./v1/plugins/admin/settings.json
-rm ./v1/plugins/healbe/settings.json
-rm ./v1/plugins/openwethermap/settings.json
-rm ./clients/.key
-rm ./clients/localstorage.json
-rm -rf ./clients/web/default/localstorage
-rm -rf ./clients/web/custom/localstorage
-rm -rf /v1/plugins/db/data
+#find ./ -name "localstorage.json" -exec rm {} \;
