42092add537dc793942093564e9b99e47358c80e
Satoshi Ikeda <ikeda@koozyt.com>
2018-02-02 19:31:55 +0900

    Add prefix '_' to private variable

--- test/plugin-interface.test.js
+++ test/plugin-interface.test.js
@@ -19,32 +19,32 @@ describe('PluginInterface', function() {
         it('should success', function() {
             new PluginInterface(
                 {}, 'abc',
-                ['server', 'client', 'http'], '.', false, STORAGEPATH);
+                ['server', 'client', 'http'], '.', STORAGEPATH);
         });
     });
     describe('server plugin', function() {
         it('should have server functions', function() {
             const pi = new PluginInterface(
-                {}, 'abc', ['server'], '.', false, STORAGEPATH);
+                {}, 'abc', ['server'], '.', STORAGEPATH);
             expect(pi.server.publish).to.be.a('function');
         });
         it('should not have client function', function() {
             const pi = new PluginInterface(
-                {}, 'abc', ['server'], '.', false, STORAGEPATH);
+                {}, 'abc', ['server'], '.', STORAGEPATH);
             expect(pi.client).to.be.undefined;
         });
     });
     describe('client plugin', function() {
         it('should have client functions', function() {
             const pi = new PluginInterface(
-                {}, 'abc', ['client'], '.', false, STORAGEPATH);
+                {}, 'abc', ['client'], '.', STORAGEPATH);
             expect(pi.client.subscribe).to.be.a('function');
             expect(pi.client.unsubscribe).to.be.a('function');
             expect(pi.client.callProc).to.be.a('function');
         });
         it('should not have server function', function() {
             const pi = new PluginInterface(
-                {}, 'abc', ['client'], '.', false, STORAGEPATH);
+                {}, 'abc', ['client'], '.', STORAGEPATH);
             expect(pi.server).to.be.undefined;
         });
     });
